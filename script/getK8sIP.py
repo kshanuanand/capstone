41369d71d647fea8f9ee3f404ed60f5031bdc536
Kumar Shanu Anand <kshanuanand5192@gmail.com>
2020-09-27 20:12:51 +0530

    Fix py

--- script/getK8sIP.py
+++ script/getK8sIP.py
@@ -8,8 +8,7 @@ responseCfn = cfn.describe_stacks(StackName=sName)
 Outputs = responseCfn['Stacks'][0]['Outputs']
 k8shost = ''
 for output in Outputs:
-    print(output)
     if output['OutputKey'] == 'Ec2IPPUB':
         k8shost = output['OutputValue']
         break;
-return k8shost
+print(k8shost)
