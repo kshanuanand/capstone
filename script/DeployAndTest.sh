307591f4a81f95b32b07b69ff65d925101f1d7ad
Kumar Shanu Anand <kshanuanand5192@gmail.com>
2020-10-04 12:02:36 +0530

    Remove python3 interpreter

--- script/DeployAndTest.sh
+++ script/DeployAndTest.sh
@@ -23,7 +23,6 @@ ${k8shost} ansible_user=ubuntu ansible_connection=ssh
 [all:vars]
 ansible_ssh_common_args='-o StrictHostKeyChecking=no'
 ansible_ssh_private_key_file=/var/lib/jenkins/.secrets/aws-cli-test.pem
-ansible_python_interpreter=usr/bin/python3
 EOF
 
 cat > infrastructure/group_vars/all/main <<EOF
